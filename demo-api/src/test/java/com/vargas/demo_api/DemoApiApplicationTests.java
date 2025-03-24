fea56ac3a83f2194e5369fa62c9a74d1e78bc987
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-24 09:07:13 -0400

    feat: first commit

---
+++ demo-api/src/test/java/com/vargas/demo_api/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vargas.demo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
