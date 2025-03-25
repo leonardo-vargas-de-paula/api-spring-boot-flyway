e98f0f5d1d66cb8db58ce12dc49f964003bc064f
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2025-03-25 17:12:03 -0400

    fix: corrige nome de RecursoNaoEncontradoException

--- src/main/java/com/vargas/demo_api/exceptions/RecursoNaoEcontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.vargas.demo_api.exceptions;
-
-public class RecursoNaoEcontradoException extends RuntimeException{
-
-    public RecursoNaoEcontradoException(String message){
-        super(message);
-    }
-}
